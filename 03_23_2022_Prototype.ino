fa346fd811e8f7a2e3f3d01f1b5bc73504ff5045
nzchen <33382166+nzchen@users.noreply.github.com>
2022-05-02 00:02:33 -0400

    added comments

--- 03_23_2022_Prototype.ino
+++ 03_23_2022_Prototype.ino
@@ -1,7 +1,8 @@
+//import libraries needed
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
- 
+ //create necessary global variables
 Adafruit_ADS1115 ads;         
 const float multiplier = 0.1875F;
 char dataStr[100] = "";
@@ -9,14 +10,16 @@ char buffer[7];
 
 void setup() {
   // put your setup code here, to run once:
+  //set up the baud rate for the arduino, matches python code
   Serial.begin(9600);
   //Serial.println("Setting Up ADC");
 
+  //attempts to initialize the adc
   if (!ads.begin()) {
     Serial.println("Failed to initialize ADS.");
     while (1);
   }
-  // Descomentar el que interese
+  // Gain values you can set
   // ads.setGain(GAIN_TWOTHIRDS);  +/- 6.144V  1 bit = 0.1875mV (default)
   // ads.setGain(GAIN_ONE);        +/- 4.096V  1 bit = 0.125mV
   // ads.setGain(GAIN_TWO);        +/- 2.048V  1 bit = 0.0625mV
@@ -24,6 +27,8 @@ void setup() {
   // ads.setGain(GAIN_EIGHT);      +/- 0.512V  1 bit = 0.015625mV
   //ads.setGain(GAIN_SIXTEEN);    //+/- 0.256V  1 bit = 0.0078125mV 
 }
+
+//start time 
 double start_time = 0;
 void loop() {
   //
@@ -33,12 +38,20 @@ void loop() {
   int16_t adc0;
   float volts0;
 
+  //read adc value
   adc0 = ads.readADC_SingleEnded(0);
+  //convert adc value to actual voltage value
   volts0 = ads.computeVolts(adc0);
+  //prints the start time
   Serial.print(start_time);
   Serial.print(" ");
+  //increments the start time
+  //currently it is set to run 10 times a second
   start_time+=0.1;
+  //prints out the voltage
   Serial.println(volts0,6);
   
+  //adc sampling at 10 times per second
+  //can alter but must also alter the increment for the start_time
   delay(100);
 }
